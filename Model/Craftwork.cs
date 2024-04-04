0eacc98752a800aa2f564df775f2234ea603ccd8
Mashudu Luvhengo <Mluvhengo25@gmail.com>
2024-04-04 18:33:39 +0200

    updated razor pages

---
+++ Model/Craftwork.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KhumaloCraft.Models
+{
+    public class Craftwork
+    {
+        [Key]
+        public int CraftId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public string ImageURL { get; set; }
+    }
+}
